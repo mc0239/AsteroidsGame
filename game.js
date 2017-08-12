859ed0783d68f5745d58edf5d4849365d0134e41
mc0239 <mc0239@student.uni-lj.si>
2017-08-12 22:15:47 +0200

    Initial commit

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var canvas, ctx, then;
 
 var a;
 
-var player, bullets;
+var player, bullets, asteroids, score;
 
 window.onload = function() {
     // init canvas
@@ -20,6 +20,7 @@ window.onload = function() {
 
     then = Date.now();
     initPlayer();
+    generateAsteroidWave();
     main();
 };
 
@@ -29,6 +30,9 @@ function loadImages() {
 
     a.img.offpointer = new Image();
     a.img.offpointer.src = "./img/offscreen_pointer.png";
+
+    a.img.offpointerAsteroid = new Image();
+    a.img.offpointerAsteroid.src = "./img/offscreen_pointer2.png";
 
     a.img.bullet1 = new Image();
     a.img.bullet1.src = "./img/bullet1.png";
@@ -42,36 +46,63 @@ function initPlayer() {
         x: canvas.width/2,
         y: canvas.height/2,
         rot: 0,
-        speed: 4,
+
+        turnSpeed: 2,
+
+        maxSpeed: 3,
+        acceleration: 0.1,
+        velocity: 0,
+
         bullet1Cooldown: 1000,
         bullet1CooldownCurrent: 0
     };
     bullets = [];
+    score = 0;
 }
 
+let angX = 0, angY = 0;
 function update(dt) {
 
     movePlayer();
+    moveAsteroids();
     bulletLogic();
+    checkCollision();
 
     function movePlayer() {
-        if(keysDown["a"] || keysDown["A"]) player.rot-=3;
-        if(keysDown["d"] || keysDown["D"]) player.rot+=3;
+        if(keysDown["a"] || keysDown["A"]) player.rot -= player.turnSpeed;
+        if(keysDown["d"] || keysDown["D"]) player.rot += player.turnSpeed;
         if(player.rot >= 360) player.rot = 0;
         if(player.rot <= -360) player.rot = 0;
 
-        let c = player.speed; // speed ?
-        let alpha = player.rot;
-        let a = Math.sin(degToRad(alpha)) * c;
-        let b = Math.cos(degToRad(alpha)) * c;
+        // TODO: REDO THIS WHOLE THING OH MY GOD PLEASE
+        let c = player.velocity; // speed ?
+        player.x += angX * c;
+        player.y -= angY * c;
 
         if(keysDown["w"] || keysDown["W"]) {
-            player.x += a;
-            player.y -= b;
-        }
-        if(keysDown["s"] || keysDown["S"]) {
-            player.x += a * -0.3;
-            player.y -= b * -0.3;
+            angX = Math.sin(degToRad(player.rot));
+            angY = Math.cos(degToRad(player.rot));
+
+            player.velocity += player.acceleration;
+            if(player.velocity > player.maxSpeed) {
+                player.velocity = player.maxSpeed;
+            }
+        } else if(keysDown["s"] || keysDown["S"]) {
+            angX = Math.sin(degToRad(player.rot));
+            angY = Math.cos(degToRad(player.rot));
+
+            player.velocity -= player.acceleration/10;
+            if(player.velocity < -player.maxSpeed) {
+                player.velocity = -player.maxSpeed;
+            }
+        } else {
+            if(player.velocity < 0) {
+                player.velocity += player.acceleration/5;
+                if(player.velocity >= 0) player.velocity = 0;
+            } else {
+                player.velocity -= player.acceleration/5;
+                if(player.velocity <= 0) player.velocity = 0;
+            }
         }
 
         // wrap around
@@ -112,6 +143,37 @@ function update(dt) {
             }
         }
     }
+
+    function moveAsteroids() {
+        asteroids.forEach(function(t, i) {
+            t.x += Math.sin(degToRad(t.rot)) * t.speed;
+            t.y -= Math.cos(degToRad(t.rot)) * t.speed;
+            // wrap around
+            if(t.x < -100) t.x = canvas.width + 100;
+            if(t.x > canvas.width+100) t.x = -100;
+            if(t.y < -100) t.y = canvas.height + 100;
+            if(t.y > canvas.height+100) t.y = -100;
+        });
+    }
+
+    function checkCollision() {
+        asteroids.forEach(function(e, i) {
+            bullets.forEach(function(b, j) {
+                if(isInCollision(e.x, e.y, 32, 32, b.x, b.y, 3, 3)) {
+                    bullets.splice(j, 1);
+                    e.health--;
+                    score += 100;
+                    if(e.health <= 0) {
+                        asteroids.splice(i, 1);
+                    }
+                }
+            });
+
+            if(isInCollision(e.x, e.y, 32, 32, player.x-16, player.y-16, 32, 32)) {
+                //TODO: IS ROCKET KILL?
+            }
+        });
+    }
 }
 
 function makeBullet() {
@@ -126,72 +188,124 @@ function makeBullet() {
 }
 
 function makeAsteroid() {
-    //TODO: make asteroids!!!
+    let asteroid = {
+        x: Math.ceil(Math.random()*1000) - 100,
+        y: Math.ceil(Math.random()*800) - 100,
+        rot: Math.ceil(Math.random()*360),
+        speed: 3,
+        health: 4
+    };
+    return asteroid;
 }
 
-// https://stackoverflow.com/questions/2677671/how-do-i-rotate-a-single-object-on-an-html-5-canvas#11985464
-function drawImageRot(img, x, y, width, height, deg){
-    // convert degrees to radian
-    var rad = degToRad(deg);
-    // set the origin to the center of the image
-    ctx.translate(x + width/2, y + height/2);
-    // rotate the canvas around the origin
-    ctx.rotate(rad);
-    //draw the image
-    ctx.drawImage(img, -width/2, -height/2, width, height);
-    //reset the canvas
-    ctx.rotate(-rad);
-    ctx.translate(-(x + width/2), -(y + height / 2));
+function generateAsteroidWave() {
+    asteroids = [];
+    for(let i=0; i<8; i++) {
+        asteroids.push(makeAsteroid());
+    }
 }
 
 function degToRad(deg) {
     return deg * Math.PI / 180;
 }
 
+function isInCollision(x1, y1, w1, h1, x2, y2, w2, h2) {
+    if (x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && h1 + y1 > y2) return true;
+    else return false;
+}
+
 function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawDebug();
+    drawScore();
 
     drawPlayer();
     drawBullets();
+    drawAsteroids();
 
-    ctx.drawImage(a.img.asteroid1small, 50, 50);
+    //ctx.drawImage(a.img.asteroid1small, 50, 50);
 
-}
+    // https://stackoverflow.com/questions/2677671/how-do-i-rotate-a-single-object-on-an-html-5-canvas#11985464
+    function drawImageRot(img, x, y, width, height, deg){
+        // convert degrees to radian
+        var rad = degToRad(deg);
+        // set the origin to the center of the image
+        ctx.translate(x + width/2, y + height/2);
+        // rotate the canvas around the origin
+        ctx.rotate(rad);
+        //draw the image
+        ctx.drawImage(img, -width/2, -height/2, width, height);
+        //reset the canvas
+        ctx.rotate(-rad);
+        ctx.translate(-(x + width/2), -(y + height / 2));
+    }
 
-function drawPlayer() {
-    drawImageRot(a.img.rocket1, player.x - 34/2, player.y - 33/2, 34, 33, player.rot);
+    function drawPlayer() {
+        drawImageRot(a.img.rocket1, player.x - 34/2, player.y - 33/2, 34, 33, player.rot);
 
-    let xm = 0, ym = 0;
-    if(player.x < 0) xm = -1;
-    if(player.x > 800) xm = 1;
-    if(player.y < 0) ym = -1;
-    if(player.y > 600) ym = 1;
+        // draw offscreen pointer
+        let xm = 0, ym = 0;
+        if(player.x < 0) xm = -1;
+        if(player.x > 800) xm = 1;
+        if(player.y < 0) ym = -1;
+        if(player.y > 600) ym = 1;
 
-    if(xm > 0 && ym > 0) drawImageRot(a.img.offpointer, canvas.width-13, canvas.height-15, 13, 15, 135);
-    else if(xm > 0 && ym < 0) drawImageRot(a.img.offpointer, canvas.width-13, 0, 13, 15, 45);
-    else if(xm < 0 && ym > 0) drawImageRot(a.img.offpointer, 0, canvas.height-15, 13, 15, -135);
-    else if(xm < 0 && ym < 0) drawImageRot(a.img.offpointer, 0, 0, 13, 15, -45);
+        if(xm > 0 && ym > 0) drawImageRot(a.img.offpointer, canvas.width-13, canvas.height-15, 13, 15, 135);
+        else if(xm > 0 && ym < 0) drawImageRot(a.img.offpointer, canvas.width-13, 0, 13, 15, 45);
+        else if(xm < 0 && ym > 0) drawImageRot(a.img.offpointer, 0, canvas.height-15, 13, 15, -135);
+        else if(xm < 0 && ym < 0) drawImageRot(a.img.offpointer, 0, 0, 13, 15, -45);
 
-    else if(xm > 0) drawImageRot(a.img.offpointer, canvas.width-13, player.y, 13, 15, 90);
-    else if(xm < 0) drawImageRot(a.img.offpointer, 0, player.y, 13, 15, -90);
+        else if(xm > 0) drawImageRot(a.img.offpointer, canvas.width-13, player.y, 13, 15, 90);
+        else if(xm < 0) drawImageRot(a.img.offpointer, 0, player.y, 13, 15, -90);
 
-    else if(ym > 0) drawImageRot(a.img.offpointer, player.x, canvas.height-13, 13, 15, 180);
-    else if(ym < 0) drawImageRot(a.img.offpointer, player.x, 0, 13, 15, 0);
-}
+        else if(ym > 0) drawImageRot(a.img.offpointer, player.x, canvas.height-13, 13, 15, 180);
+        else if(ym < 0) drawImageRot(a.img.offpointer, player.x, 0, 13, 15, 0);
+    }
 
-function drawBullets() {
-    bullets.forEach(function(b) {
-        drawImageRot(a.img.bullet1, b.x, b.y, 3, 3, player.rot);
-    });
-}
+    function drawBullets() {
+        bullets.forEach(function(b) {
+            drawImageRot(a.img.bullet1, b.x, b.y, 3, 3, player.rot);
+        });
+    }
 
-function drawDebug() {
-    ctx.fillStyle = "white";
-    ctx.font = "10px sans-serif";
-    ctx.fillText(Math.round(player.x*1000)/1000, 2, 12);
-    ctx.fillText(Math.round(player.y*1000)/1000, 2, 24);
+    function drawAsteroids() {
+        asteroids.forEach(function(t) {
+            drawImageRot(a.img.asteroid1small, t.x, t.y, 32, 32, t.rot);
+
+            // draw offscreen pointer
+            let xm = 0, ym = 0;
+            if(t.x < 0) xm = -1;
+            if(t.x > 800) xm = 1;
+            if(t.y < 0) ym = -1;
+            if(t.y > 600) ym = 1;
+
+            if(xm > 0 && ym > 0) drawImageRot(a.img.offpointerAsteroid, canvas.width-9, canvas.height-9, 9, 9, 135);
+            else if(xm > 0 && ym < 0) drawImageRot(a.img.offpointerAsteroid, canvas.width-9, 0, 9, 9, 45);
+            else if(xm < 0 && ym > 0) drawImageRot(a.img.offpointerAsteroid, 0, canvas.height-9, 9, 9, -135);
+            else if(xm < 0 && ym < 0) drawImageRot(a.img.offpointerAsteroid, 0, 0, 9, 9, -45);
+
+            else if(xm > 0) drawImageRot(a.img.offpointerAsteroid, canvas.width-9, t.y, 9, 9, 90);
+            else if(xm < 0) drawImageRot(a.img.offpointerAsteroid, 0, t.y, 9, 9, -90);
+
+            else if(ym > 0) drawImageRot(a.img.offpointerAsteroid, t.x, canvas.height-9, 9, 9, 180);
+            else if(ym < 0) drawImageRot(a.img.offpointerAsteroid, t.x, 0, 9, 9, 0);
+        });
+    }
+
+    function drawScore() {
+        ctx.fillStyle = "white";
+        ctx.font = "12px sans-serif";
+        ctx.fillText(score, canvas.width-canvas.width/4, 14);
+    }
+
+    function drawDebug() {
+        ctx.fillStyle = "white";
+        ctx.font = "8px sans-serif";
+        ctx.fillText(Math.round(player.x*1000)/1000, 2, 10);
+        ctx.fillText(Math.round(player.y*1000)/1000, 2, 20);
+    }
+
 }
 
 function main() {
